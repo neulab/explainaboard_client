384aee57673d39fd196445e9d119d708f3b7bb69
Lyuyang Hu <lyuyangh@andrew.cmu.edu>
2022-04-27 22:31:11 -0400

    feat: setup & precommit

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup, find_packages
 import codecs
-from version import __version__
 import os
 
+from setuptools import find_packages, setup
+
+from version import __api_client_version__, __version__
+
 setup(
-    name="explainaboard",
+    name="explainaboard_cli",
     version=__version__,
     description="Explainable CLI",
     long_description=codecs.open("README.md", encoding="utf-8").read(),
@@ -16,14 +18,24 @@ setup(
         "Topic :: Text Processing",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
     ],
     packages=find_packages(),
     entry_points={
-        "console_scripts": [
-        ],
+        "console_scripts": [],
     },
     install_requires=[],
+    extras_require={
+        "dev": [
+            "pre-commit",
+        ],
+    },
     include_package_data=True,
 )
-os.system("pip install https://github.com/neulab/explainaboard_web/releases/download/latest/explainaboard_api_client-1.0.0-py3-none-any.whl")
+os.system(
+    "pip install https://github.com/neulab/explainaboard_web/releases"
+    f"/download/{__api_client_version__}"
+    "/explainaboard_api_client-1.0.0-py3-none-any.whl"
+)
