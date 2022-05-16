f1f5b5bf9bf44dbc02b1c3ef8f8e991251815f88
Graham Neubig <neubig@gmail.com>
2022-05-15 21:17:25 -0400

    Add CLI for uploading systems

---
+++ explainaboard_client/tasks.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import json
+from enum import Enum
+
+#### Copy-pasted from ExplainaBoard constants.py
+class TaskType(str, Enum):
+    text_classification = "text-classification"
+    named_entity_recognition = "named-entity-recognition"
+    qa_extractive = "qa-extractive"
+    summarization = "summarization"
+    machine_translation = "machine-translation"
+    text_pair_classification = "text-pair-classification"
+    aspect_based_sentiment_classification = "aspect-based-sentiment-classification"
+    kg_link_tail_prediction = "kg-link-tail-prediction"
+    qa_multiple_choice = "qa-multiple-choice"
+    conditional_generation = "conditional-generation"
+    word_segmentation = "word-segmentation"
+    language_modeling = "language-modeling"
+    chunking = "chunking"
+
+    @staticmethod
+    def list():
+        return list(map(lambda c: c.value, TaskType))
+
+class FileType(str, Enum):
+    json = "json"
+    tsv = "tsv"
+    csv = "csv"
+    conll = "conll"  # for tagging task such as named entity recognition
+    datalab = "datalab"
+    text = "text"
+
+    @staticmethod
+    def list():
+        return list(map(lambda c: c.value, FileType))
+#### End copy-paste from ExplainaBoard
+
+
+DEFAULT_METRICS: dict[TaskType,list[str]] = {
+    TaskType.text_classification: ['Accuracy'],
+    TaskType.named_entity_recognition: ['BIOF1Score'],
+    TaskType.qa_extractive: ['ExactMatchQA', 'F1ScoreQA'],
+    TaskType.summarization: ['rouge1', 'rouge2', 'rougeL'],
+    TaskType.machine_translation: ['bleu'],
+    TaskType.text_pair_classification: ['Accuracy'],
+    TaskType.aspect_based_sentiment_classification: ['Accuracy'],
+    TaskType.kg_link_tail_prediction: ['Hits', 'MRR'],
+    TaskType.qa_multiple_choice: ['Accuracy'],
+    TaskType.conditional_generation: ['bleu'],
+    TaskType.word_segmentation: ['BMESF1Score'],
+    TaskType.language_modeling: ['LogProb'],
+    TaskType.chunking: ['Accuracy'],
+}
+
+
+FILE_SUFFIX_MAP = {'txt': 'text'}
+def infer_file_type(file_path: str, task: TaskType):
+    """
+    Infer the type of the file from the file path and task type. Mostly looks at the
+    suffix, tries to parse json and returns json if it works, then gives up otherwise.
+    """
+    suffix = file_path.split('.')[-1]
+    type_str = FILE_SUFFIX_MAP.get(suffix,suffix)
+    if type_str in FileType.list():
+        return type_str
+    else:
+        try:
+            with open(file_path, 'r') as fin:
+                json.load(fin)
+            return 'json'
+        except:
+            raise ValueError(f'Could not infer file type of {file_path}. Please '
+                             'specify the file type directly via the command line')
---
+++ explainaboard_client/upload_system.py
@@ -0,0 +1,113 @@
+import argparse
+from typing import get_args
+
+from datalabs.info import PaperInfo
+from explainaboard_api_client.model.system import System
+from explainaboard_api_client.model.system_create_props import SystemCreateProps
+from explainaboard_api_client.model.system_metadata import SystemMetadata
+from explainaboard_api_client.model.system_output_props import SystemOutputProps
+
+from explainaboard_client import Config, ExplainaboardClient
+from explainaboard_client.tasks import TaskType, DEFAULT_METRICS, infer_file_type, \
+    FileType
+from explainaboard_client.utils import generate_dataset_id
+
+
+def main():
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='A command-line tool to upload system '
+                                                 'to the ExplainaBoard web interface.')
+    parser.add_argument('--email', type=str, required=True,
+                        help='Email address used to sign in to ExplainaBoard')
+    parser.add_argument('--api_key', type=str, required=True,
+                        help='Your API key')
+    parser.add_argument('--task', type=str, required=True,
+                        choices=TaskType.list(),
+                        help='What task you will be analyzing')
+    parser.add_argument('--model_name', type=str, required=True,
+                        help='Name of the system that you are uploading')
+    parser.add_argument('--system_output', type=str, required=True,
+                        help='Path to the system output file')
+    parser.add_argument('--output_file_type', type=str,
+                        choices=FileType.list(),
+                        help='File type of the system output (eg text/json/tsv/conll)')
+    dataset_group = parser.add_mutually_exclusive_group(required=True)
+    dataset_group.add_argument('--dataset', type=str,
+                               help='A dataset name from DataLab')
+    parser.add_argument('--sub_dataset', type=str, required=False,
+                        help='A sub-dataset name from DataLab')
+    parser.add_argument('--split', type=str, required=False, default='test',
+                        help='The name of the dataset split to process')
+    dataset_group.add_argument('--custom_dataset', type=str,
+                               help='The path to a custom dataset file')
+    parser.add_argument('--custom_dataset_file_type', type=str, required=False,
+                        choices=FileType.list(),
+                        help='File type of the custom dataset (eg text/json/tsv/conll)')
+    parser.add_argument('--metric_names', type=str, nargs='+', required=False,
+                        help='The metrics to compute, leave blank for task defaults')
+    parser.add_argument('--source_language', type=str,
+                        help='The language on the input side')
+    parser.add_argument('--target_language', type=str,
+                        help='The language on the output side')
+    parser.add_argument('--public', action='store_true',
+                        help='Make the uploaded system public')
+    parser.add_argument('--server', type=str, required=False, default='main',
+                        choices=get_args(Config.environment),
+                        help='Which server to upload to, "main" should be sufficient')
+    args = parser.parse_args()
+
+    # Sanity checks
+    if not (args.source_language or args.target_language):
+        raise ValueError(f'You must specify source and/or target language')
+
+    # Infer missing values
+    task = TaskType(args.task)
+    metric_names = args.metric_names or DEFAULT_METRICS[args.task]
+    source_language = args.source_language or args.target_language
+    target_language = args.target_language or args.source_language
+    output_file_type = args.output_file_type or infer_file_type(args.system_output, task)
+    custom_dataset_file_type = args.custom_dataset_file_type or infer_file_type(args.custom_dataset_file_type, task)
+
+    # Do the actual upload
+    system_output = SystemOutputProps(
+        data=args.system_output,
+        file_type=output_file_type,
+    )
+    metadata = SystemMetadata(
+        task=args.task,
+        is_private=not args.public,
+        model_name=args.model_name,
+        metric_names=metric_names,
+        source_language=source_language,
+        target_language=target_language,
+        dataset_metadata_id=generate_dataset_id(args.dataset, args.sub_dataset),
+        dataset_split=args.split,
+        paper_info=PaperInfo(),  # all attributes are optional
+    )
+    custom_dataset = None
+    if args.custom_dataset:
+        custom_dataset = SystemOutputProps(
+            data=args.custom_dataset,
+            file_type=custom_dataset_file_type,
+        )
+    create_props = SystemCreateProps(
+        metadata=metadata, system_output=system_output, custom_datset=custom_dataset
+    )
+    client_config = Config(
+        args.email,
+        args.api_key,
+        args.server,
+    )
+    client = ExplainaboardClient(client_config)
+
+    result: System = client.systems_post(create_props)
+    try:
+        sys_id = result.system_id
+        sys = client.systems_system_id_get(sys_id)
+        print(f'successfully posted system {args.model_name} with ID {sys_id}')
+    except:
+        print(f'failed to post system {args.model_name}')
+
+if __name__ == '__main__':
+    main()
