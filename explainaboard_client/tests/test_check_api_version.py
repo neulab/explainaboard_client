ad5b90e5c12799ac7cc9cf93f500863e892ecbb9
OscarWang114 <chihhaowang.oscar@gmail.com>
2022-12-02 00:12:53 -0500

    Add test

--- explainaboard_client/tests/test_check_api_version.py
+++ explainaboard_client/tests/test_check_api_version.py
@@ -6,21 +6,23 @@ from unittest.mock import patch
 from explainaboard_api_client.exceptions import ApiException
 import explainaboard_client
 from explainaboard_client.cli import delete_systems
+from explainaboard_client.client import ExplainaboardClient
 from explainaboard_client.exceptions import APIVersionMismatchException
 from explainaboard_client.tests.test_utils import TestEndpointsE2E
 
 
+def _with_wrong_api_version(func: Callable) -> Callable:
+    def wrapper(self, *args, **kwargs):
+        prev_version = self._client._api_client_version
+        self._client._api_client_version = "0.0.0"
+        result = func(self, *args, **kwargs)
+        self._client._api_client_version = prev_version
+        return result
+
+    return wrapper
+
+
 class TestCheckAPIVersion(TestEndpointsE2E):
-    def _with_wrong_api_version(func: Callable) -> Callable:
-        def wrapper(self, *args, **kwargs):
-            prev_version = self._client._api_client_version
-            self._client._api_client_version = "0.0.0"
-            result = func(self, *args, **kwargs)
-            self._client._api_client_version = prev_version
-            return result
-
-        return wrapper
-
     def test_check_api_version_match(self):
         info = self._client.info_get()
         self.assertEqual(info["api_version"], self._client._api_client_version)
@@ -58,3 +60,16 @@ class TestCheckAPIVersion(TestEndpointsE2E):
                     (ApiException, APIVersionMismatchException, SystemExit)
                 ):
                     delete_systems.main()
+
+
+class TestCheckAPIVersionDisabled(TestEndpointsE2E):
+    def setUp(self):
+        super().setUp()
+        # disable API version checking
+        explainaboard_client.check_api_version = False
+        self._client = ExplainaboardClient()
+
+    @_with_wrong_api_version
+    def test_check_api_version_disabled(self):
+        # should execute with no exception raised
+        _ = self._client.info_get()
